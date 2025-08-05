8330781d6177011876493c208a07697b2a0d6045
Lucas Farias <lucas.farias@rtbhouse.com>
2025-08-05 15:12:54 +0000

    feat: add pino to log

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,13 +2,14 @@ import sdk from './tracer';
 sdk.start();
 
 import { NestFactory } from '@nestjs/core';
+import { log } from 'src/infra/logger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 3001).then(() => {
-    console.log('TESTE');
+    log.info('TESTE');
   });
 }
 
