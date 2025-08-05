c5214361c3bebcf5f2495e5b51b8d75f35536e3e
Lucas Farias <lucas.farias@rtbhouse.com>
2025-08-05 20:46:57 +0000

    feat: add prometheus

--- app/src/tracer.ts
+++ app/src/tracer.ts
@@ -1,27 +1,29 @@
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
-import {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} from '@opentelemetry/sdk-metrics';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import {
-  BatchSpanProcessor,
-  ConsoleSpanExporter,
-} from '@opentelemetry/sdk-trace-node';
 import {
   ATTR_SERVICE_NAME,
   ATTR_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
 
+const SERVICE_NAME = 'app-rs-dev-ops-observability';
+
+const metricsExporter = new OTLPMetricExporter({
+  url: 'http://127.0.0.1:4317',
+});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricsExporter,
+  exportIntervalMillis: 10 * 1000, // 10s
+});
+
 const traceExporter = new OTLPTraceExporter();
 
 const resource = resourceFromAttributes({
-  [ATTR_SERVICE_NAME]: 'app-rs-dev-ops-observability',
+  [ATTR_SERVICE_NAME]: SERVICE_NAME,
   [ATTR_SERVICE_VERSION]: '1.0.0',
 });
 
@@ -29,14 +31,11 @@ const mergedResource = resource;
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter),
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
-  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
+  traceExporter,
+  metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
   resource: mergedResource,
+  serviceName: SERVICE_NAME,
 });
 
 export default sdk;
