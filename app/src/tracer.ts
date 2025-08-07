77fc599f477a137ac0f4afe925634bff25fdcf18
Lucas Farias <lucas.farias@rtbhouse.com>
2025-08-07 18:44:12 +0000

    feat: add custom metrics

--- app/src/tracer.ts
+++ app/src/tracer.ts
@@ -1,9 +1,9 @@
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { diag, DiagConsoleLogger, DiagLogLevel, metrics } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import {
   ATTR_SERVICE_NAME,
@@ -32,10 +32,16 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new NodeSDK({
   traceExporter,
-  metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
   resource: mergedResource,
   serviceName: SERVICE_NAME,
 });
 
-export default sdk;
+const meterProvider = new MeterProvider({
+  resource: mergedResource,
+  readers: [metricReader]
+})
+metrics.setGlobalMeterProvider(meterProvider)
+
+export { metrics, sdk, SERVICE_NAME };
+
