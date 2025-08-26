ef1a0c3770ed447a95572318f89048379d946fb4
Lucas Farias <lucas.farias@rtbhouse.com>
2025-08-26 13:51:57 +0000

    feat: create deployment v2

--- service-mesh/app/src/domain/health/health.service.ts
+++ service-mesh/app/src/domain/health/health.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
-    console.log('Checked application health!');
-    return 'OK';
+    console.log('Checked application health! - V2');
+    return 'OK - V2';
   }
 
   checkReady(): string {
-    console.log('Checked application readiness!');
-    return 'OK';
+    console.log('Checked application readiness! - V2');
+    return 'OK - V2';
   }
 }
